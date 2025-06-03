4d631805412ddc0bd84e383649b44c4ff32cd4b6
mullzalt <rizki.d.mulya@gmail.com>
2025-06-03 12:06:03 +0700

    chore: upadate the config js

--- server/config/default.js
+++ server/config/default.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    access_token_expires_in: 5,
-    refresh_token_expires_in: 720,
+  access_token_expires_in: 5,
+  refresh_token_expires_in: 720,
 };
